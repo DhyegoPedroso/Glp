a323b5e0795377b43ffa276a54892302c36e9e55
Dhyego Pedroso <dhyego.pedroso@outlook.com>
2019-10-07 23:27:12 -0300

    Testando metodos da tela Cliente

--- src/java/br/com/glp/controller/ClienteControle.java
+++ src/java/br/com/glp/controller/ClienteControle.java
@@ -11,7 +11,6 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.model.DataModel;
@@ -27,40 +26,155 @@ import org.hibernate.Session;
 @ViewScoped
 public class ClienteControle implements Serializable {
 
-//    private Session session;
-//    private boolean mostrar_toolbar;
-//    private boolean mostrarTabelaEndereco;
-//    private boolean mostrarTabelaCaminhao;
-//
-//    private ClienteDao clienteDao;
-//
-//    private Endereco endereco;
-//    private List<Endereco> enderecos;
-//    private DataModel<Endereco> modelEnderecos;
-//
-//    private Cliente cliente;
-//    private List<Cliente> clientes;
-//    private DataModel<Cliente> modelClientes;
-//
-//    private Caminhao caminhao;
-//    private List<Caminhao> caminhoes;
-//    private DataModel<Caminhao> modelCaminhoes;
-//
-//    private Contato contato;
+    private Session session;
+    private boolean mostrar_toolbar;
+    private boolean mostrarTabelaEndereco;
+    private boolean mostrarTabelaCaminhao;
+
+    private ClienteDao clienteDao;
+
+    private Endereco endereco;
+    private List<Endereco> enderecos;
+    private DataModel<Endereco> modelEnderecos;
+
     private Cliente cliente;
-
     private List<Cliente> clientes;
+    private DataModel<Cliente> modelClientes;
 
-    public Cliente addCliente() {
-        if (clientes.contains(cliente)) {
-            Mensagem.campoExiste("Endereço ");
-        } else {
-            clientes.add(cliente);
-            cliente = new Cliente();
-        }
-        return null;
+    private Caminhao caminhao;
+    private List<Caminhao> caminhoes;
+    private DataModel<Caminhao> modelCaminhoes;
+
+    private Contato contato;
+
+
+    public ClienteControle() {
+        clienteDao = new ClienteDaoImpl();
     }
 
+    private void abreSessao() {
+        if (session == null) {
+            session = HibernateUtil.abreSessao();
+        } else if (!session.isOpen()) {
+            session = HibernateUtil.abreSessao();
+        }
+    }
+
+    public void novo() {
+        mostrar_toolbar = !mostrar_toolbar;
+        limpar();
+    }
+
+    public void novaPesquisa() {
+        mostrar_toolbar = !mostrar_toolbar;
+        limpar();
+    }
+
+    public void preparaAlterar() {
+        mostrar_toolbar = !mostrar_toolbar;
+        limpar();
+    }
+
+    public void carregarParaAlterar() {
+        mostrar_toolbar = !mostrar_toolbar;
+        cliente = modelClientes.getRowData();
+        enderecos = cliente.getEnderecos();
+    }
+
+    public void carregarTabelaEndereco() {
+        endereco = modelEnderecos.getRowData();
+        enderecos = cliente.getEnderecos();
+    }
+
+    public void carregarTabelaCaminhao() {
+        caminhao = modelCaminhoes.getRowData();
+
+    }
+
+    public void pesquisar() {
+        clienteDao = new ClienteDaoImpl();
+        try {
+            abreSessao();
+
+            if (!cliente.getNome().equals("")) {
+                clientes = clienteDao.pesquisaPorNome(cliente.getNome(), session);
+            } else {
+                Mensagem.campoVazio("O campo Cliente");
+            }
+
+            modelClientes = new ListDataModel(clientes);
+        } catch (HibernateException ex) {
+            System.err.println("Erro pesquisa Cliente:\n" + ex.getMessage());
+        } finally {
+            session.close();
+        }
+    }
+
+    public void limpar() {
+        cliente = new Cliente();
+        contato = new Contato();
+        endereco = new Endereco();
+        caminhao = new Caminhao();
+    }
+
+    public void excluir() {
+        cliente = modelClientes.getRowData();
+        abreSessao();
+        try {
+            clienteDao.remover(cliente, session);
+            clientes.remove(cliente);
+            modelClientes = new ListDataModel(clientes);
+            Mensagem.excluir("Cliente");
+            limpar();
+        } catch (Exception e) {
+            System.out.println("erro ao excluir: " + e.getMessage());
+        } finally {
+            session.close();
+        }
+    }
+
+    public void salvar() {
+        try {
+            abreSessao();
+
+            if (cliente.getId() == null) {
+                cliente.setDtCadastro(new Date());
+            }
+
+            clienteDao.salvarOuAlterar(cliente, session);
+            Mensagem.salvar("Cliente: " + cliente.getNome());
+            cliente = null;
+            endereco = null;
+            contato = null;
+
+        } catch (HibernateException ex) {
+            System.err.println("Erro ao Salvar Cliente:\n" + ex.getMessage());
+        } catch (Exception e) {
+            System.out.println("Erro no salvar clienteDao Controle "
+                    + e.getMessage());
+        } finally {
+            session.close();
+        }
+        limpar();
+    }
+
+    public void addEndereco(){
+        enderecos.add(endereco);
+        endereco = new Endereco();
+    }
+    
+    public void addCaminhao(){
+        caminhoes.add(caminhao);
+        caminhao = new Caminhao();
+    }
+
+    public void limparTela() {
+        limpar();
+    }
+
+    //    
+    //Getters e Setters
+    //
     public Cliente getCliente() {
         if (cliente == null) {
             cliente = new Cliente();
@@ -68,297 +182,126 @@ public class ClienteControle implements Serializable {
         return cliente;
     }
 
-    public List<Cliente> getClientes() {
-        if (clientes == null) {
-            clientes = new ArrayList<>();
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Endereco getEndereco() {
+        if (endereco == null) {
+            endereco = new Endereco();
         }
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public Caminhao getCaminhao() {
+        if (caminhao == null) {
+            caminhao = new Caminhao();
+        }
+        return caminhao;
+    }
+
+    public void setCaminhao(Caminhao caminhao) {
+        this.caminhao = caminhao;
+    }
+
+    public DataModel<Cliente> getModelClientes() {
+        return modelClientes;
+    }
+
+    public void setModelClientes(DataModel<Cliente> modelClientes) {
+        this.modelClientes = modelClientes;
+    }
+
+    public DataModel<Endereco> getModelEnderecos() {
+        return modelEnderecos;
+    }
+
+    public void setModelEnderecos(DataModel<Endereco> modelEnderecos) {
+        this.modelEnderecos = modelEnderecos;
+    }
+
+    public DataModel<Caminhao> getModelCaminhoes() {
+        return modelCaminhoes;
+    }
+
+    public void setModelCaminhoes(DataModel<Caminhao> modelCaminhoes) {
+        this.modelCaminhoes = modelCaminhoes;
+    }
+
+    public List<Cliente> getClientes() {
         return clientes;
     }
 
-//    public ClienteControle() {
-//        clienteDao = new ClienteDaoImpl();
-//    }
-//
-//    private void abreSessao() {
-//        if (session == null) {
-//            session = HibernateUtil.abreSessao();
-//        } else if (!session.isOpen()) {
-//            session = HibernateUtil.abreSessao();
-//        }
-//    }
-//
-//    public boolean isAddCaminhao() {
-//        return this.endereco == null;
-//    }
-//
-//    public void novo() {
-//        mostrar_toolbar = !mostrar_toolbar;
-//        limpar();
-//    }
-//
-//    public void novaPesquisa() {
-//        mostrar_toolbar = !mostrar_toolbar;
-//        limpar();
-//    }
-//
-//    public void preparaAlterar() {
-//        mostrar_toolbar = !mostrar_toolbar;
-//        limpar();
-//    }
-//
-//    public void carregarParaAlterar() {
-//        mostrar_toolbar = !mostrar_toolbar;
-//        cliente = modelClientes.getRowData();
-//        enderecos = cliente.getEnderecos();
-//    }
-//
-//    public void carregarTabelaEndereco() {
-//        endereco = modelEnderecos.getRowData();
-//    }
-//
-//    public void carregarTabelaCaminhao() {
-//        caminhao = modelCaminhoes.getRowData();
-//
-//    }
-//
-//    public void pesquisar() {
-//        clienteDao = new ClienteDaoImpl();
-//        try {
-//            abreSessao();
-//
-//            if (!cliente.getNome().equals("")) {
-//                clientes = clienteDao.pesquisaPorNome(cliente.getNome(), session);
-//            } else {
-//                Mensagem.campoVazio("O campo Cliente");
-//            }
-//
-//            modelClientes = new ListDataModel(clientes);
-//        } catch (HibernateException ex) {
-//            System.err.println("Erro pesquisa Cliente:\n" + ex.getMessage());
-//        } finally {
-//            session.close();
-//        }
-//    }
-//
-//    public void limpar() {
-//        cliente = new Cliente();
-//        contato = new Contato();
-//        endereco = new Endereco();
-//        caminhao = new Caminhao();
-//    }
-//
-//    public void excluir() {
-//        cliente = modelClientes.getRowData();
-//        abreSessao();
-//        try {
-//            clienteDao.remover(cliente, session);
-//            clientes.remove(cliente);
-//            modelClientes = new ListDataModel(clientes);
-//            Mensagem.excluir("Cliente");
-//            limpar();
-//        } catch (Exception e) {
-//            System.out.println("erro ao excluir: " + e.getMessage());
-//        } finally {
-//            session.close();
-//        }
-//    }
-//
-//    public void salvar() {
-//        try {
-//            abreSessao();
-//
-//            if (cliente.getId() == null) {
-//                cliente.setDtCadastro(new Date());
-//            }
-//
-//            clienteDao.salvarOuAlterar(cliente, session);
-//            Mensagem.salvar("Cliente: " + cliente.getNome());
-//            cliente = null;
-//            endereco = null;
-//            contato = null;
-//
-//        } catch (HibernateException ex) {
-//            System.err.println("Erro ao Salvar Cliente:\n" + ex.getMessage());
-//        } catch (Exception e) {
-//            System.out.println("Erro no salvar clienteDao Controle "
-//                    + e.getMessage());
-//        } finally {
-//            session.close();
-//        }
-//        limpar();
-//    }
-//
-//    public void iniciaSalvamentoEndereco() {
-//
-//        if (enderecos == null) {
-//            enderecos = new ArrayList<>();
-//        }
-//
-//        try {
-//        } catch (Exception e) {
-//            System.out.println("Erro oa iniciar Salvamento do endereço " + e.getMessage());
-//        } finally {
-//            session.close();
-//        }
-//    }
-//
-//    public void addEndereco() {
-//
-//        if (enderecos == null) {
-//            enderecos = new ArrayList<>();
-//        }
-//        enderecos.add(endereco);
-//    }
-//
-//    public void createNew() {
-//        if (enderecos.contains(endereco)) {
-//            Mensagem.campoExiste("Endereço ");
-//        } else {
-//            enderecos.add(endereco);
-//            endereco = new Endereco();
-//        }
-//    }
-//
-//    public String reinit() {
-//        endereco = new Endereco();
-//
-//        return null;
-//    }
-//
-//    public void limparTela() {
-//        limpar();
-//    }
-//
-//    //    
-//    //Getters e Setters
-//    //
-//    public Cliente getCliente() {
-//        if (cliente == null) {
-//            cliente = new Cliente();
-//        }
-//        return cliente;
-//    }
-//
-//    public void setCliente(Cliente cliente) {
-//        this.cliente = cliente;
-//    }
-//
-//    public Endereco getEndereco() {
-//        if (endereco == null) {
-//            endereco = new Endereco();
-//        }
-//        return endereco;
-//    }
-//
-//    public void setEndereco(Endereco endereco) {
-//        this.endereco = endereco;
-//    }
-//
-//    public Caminhao getCaminhao() {
-//        if (caminhao == null) {
-//            caminhao = new Caminhao();
-//        }
-//        return caminhao;
-//    }
-//
-//    public void setCaminhao(Caminhao caminhao) {
-//        this.caminhao = caminhao;
-//    }
-//
-//    public DataModel<Cliente> getModelClientes() {
-//        return modelClientes;
-//    }
-//
-//    public void setModelClientes(DataModel<Cliente> modelClientes) {
-//        this.modelClientes = modelClientes;
-//    }
-//
-//    public DataModel<Endereco> getModelEnderecos() {
-//        return modelEnderecos;
-//    }
-//
-//    public void setModelEnderecos(DataModel<Endereco> modelEnderecos) {
-//        this.modelEnderecos = modelEnderecos;
-//    }
-//
-//    public DataModel<Caminhao> getModelCaminhoes() {
-//        return modelCaminhoes;
-//    }
-//
-//    public void setModelCaminhoes(DataModel<Caminhao> modelCaminhoes) {
-//        this.modelCaminhoes = modelCaminhoes;
-//    }
-//
-//    public List<Cliente> getClientes() {
-//        return clientes;
-//    }
-//
-//    public void setFuncionarios(List<Cliente> clientes) {
-//        this.clientes = clientes;
-//    }
-//
-//    public boolean isMostrar_toolbar() {
-//        return mostrar_toolbar;
-//    }
-//
-//    public void setMostrar_toolbar(boolean mostrar_toolbar) {
-//        this.mostrar_toolbar = mostrar_toolbar;
-//    }
-//
-//    public boolean isMostrarTabelaEndereco() {
-//        return mostrarTabelaEndereco;
-//    }
-//
-//    public void setMostrarTabelaEndereco(boolean mostrarTabelaEndereco) {
-//        this.mostrarTabelaEndereco = mostrarTabelaEndereco;
-//    }
-//
-//    public boolean isMostrarTabelaCaminhao() {
-//        return mostrarTabelaCaminhao;
-//    }
-//
-//    public void setMostrarTabelaCaminhao(boolean mostrarTabelaCaminhao) {
-//        this.mostrarTabelaCaminhao = mostrarTabelaCaminhao;
-//    }
-//
-//    public Session getSession() {
-//        return session;
-//    }
-//
-//    public void setSession(Session session) {
-//        this.session = session;
-//    }
-//
-//    public Contato getContato() {
-//        if (contato == null) {
-//            contato = new Contato();
-//        }
-//        return contato;
-//    }
-//
-//    public void setContato(Contato contato) {
-//        this.contato = contato;
-//    }
-//
-//    public List<Endereco> getEnderecos() {
-//        if (enderecos == null) {
-//            enderecos = new ArrayList<>();
-//        }
-//        return enderecos;
-//    }
-//
-//    public void setEnderecos(List<Endereco> enderecos) {
-//        this.enderecos = enderecos;
-//    }
-//
-//    public List<Caminhao> getCaminhoes() {
-//        if (caminhoes == null) {
-//            caminhoes = new ArrayList<>();
-//        }
-//        return caminhoes;
-//    }
-//
-//    public void setCaminhoes(List<Caminhao> caminhoes) {
-//        this.caminhoes = caminhoes;
-//    }
+    public void setFuncionarios(List<Cliente> clientes) {
+        this.clientes = clientes;
+    }
+
+    public boolean isMostrar_toolbar() {
+        return mostrar_toolbar;
+    }
+
+    public void setMostrar_toolbar(boolean mostrar_toolbar) {
+        this.mostrar_toolbar = mostrar_toolbar;
+    }
+
+    public boolean isMostrarTabelaEndereco() {
+        return mostrarTabelaEndereco;
+    }
+
+    public void setMostrarTabelaEndereco(boolean mostrarTabelaEndereco) {
+        this.mostrarTabelaEndereco = mostrarTabelaEndereco;
+    }
+
+    public boolean isMostrarTabelaCaminhao() {
+        return mostrarTabelaCaminhao;
+    }
+
+    public void setMostrarTabelaCaminhao(boolean mostrarTabelaCaminhao) {
+        this.mostrarTabelaCaminhao = mostrarTabelaCaminhao;
+    }
+
+    public Session getSession() {
+        return session;
+    }
+
+    public void setSession(Session session) {
+        this.session = session;
+    }
+
+    public Contato getContato() {
+        if (contato == null) {
+            contato = new Contato();
+        }
+        return contato;
+    }
+
+    public void setContato(Contato contato) {
+        this.contato = contato;
+    }
+
+    public List<Endereco> getEnderecos() {
+        if (enderecos == null) {
+            enderecos = new ArrayList<>();
+        }
+        return enderecos;
+    }
+
+    public void setEnderecos(List<Endereco> enderecos) {
+        this.enderecos = enderecos;
+    }
+
+    public List<Caminhao> getCaminhoes() {
+        if (caminhoes == null) {
+            caminhoes = new ArrayList<>();
+        }
+        return caminhoes;
+    }
+
+    public void setCaminhoes(List<Caminhao> caminhoes) {
+        this.caminhoes = caminhoes;
+    }
 }
